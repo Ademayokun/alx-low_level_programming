5e297e7353e8c45f427ac4731710aca27101da71
Ademayokun <dainiademayokun@gmail.com>
2022-04-12 14:07:32 -0700

    task 0 correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - entry point
  * return : 1
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
