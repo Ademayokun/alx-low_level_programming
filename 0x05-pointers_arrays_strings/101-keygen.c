cb90e8376d3dbcc701264da124ade3192bbe8ef1
Ademayokun <dainiademayokun@gmail.com>
2022-04-20 16:18:45 -0700

    task 10 - 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* desc - to generate random passwords
+* main - entry point
+* return: 0
+*/
+
+int password(int password_length)
+{
+    char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+    printf("\t");
+
+    for(int i = 0; i < password_length; i++)
+        {
+            printf("*");
+        }
+    printf("\n");
+    printf("\t");
+    srand(time(NULL));
+    for(int i = 0; i < password_length; i++)
+    {
+        printf("%c", list[rand() % (sizeof list - 1)]);
+    }
+    printf("\n");
+    printf("\t");
+    for(int i = 0; i < password_length; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+int password_length = 21;
+
+password(password_length);
+return 0;
+}
